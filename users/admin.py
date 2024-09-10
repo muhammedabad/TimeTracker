db6bfb99504b38fe81b2f1db2a4b69e38130c012
Muhammed Abad <muhammed@tangentsolutions.co.za>
2024-09-10 16:26:47 +0200

    User Permissions

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import Group
 from unfold.admin import ModelAdmin
-from unfold.forms import UserCreationForm
+from unfold.forms import AdminPasswordChangeForm as UnfoldAdminPasswordChangeForm
+from unfold.forms import UserCreationForm, UserChangeForm
 from unfold.widgets import UnfoldAdminPasswordInput
 
 from users.models import User
@@ -13,7 +13,7 @@ from users.models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = '__all__'
+        fields = ("password1", "password2", "rise_api_key")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,25 +24,57 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['password'].widget = UnfoldAdminPasswordInput()
+        fields = ('rise_api_key', 'rise_user_id', 'jira_api_key')
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ModelAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
+    change_password_form = UnfoldAdminPasswordChangeForm
     model = User
-    list_display = ['username', 'email', 'is_staff', 'is_active']
+    list_display = ['username', 'email', 'is_staff', 'is_active', ]
     list_filter = ['is_staff', 'is_active']
     search_fields = ['username', 'email']
     ordering = ['username']
 
+    def get_fieldsets(self, request, obj=None):
+        """
+        Return the fieldsets to use in the admin form.
+        """
+        if request.user.is_superuser:
+            return (
+                (None, {'fields': ('username', 'password')}),
+                ('Personal info',
+                 {'fields': ('email', 'first_name', 'last_name', 'rise_api_key', 'rise_user_id', 'jira_api_key')}),
+                ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')}),
+                ('Important dates', {'fields': ('last_login', 'date_joined')}),
+            )
+        else:
+            return (
+                (None, {'fields': ('username', 'password')}),
+                ('Personal info',
+                 {'fields': ('email', 'first_name', 'last_name', 'rise_api_key', 'rise_user_id', 'jira_api_key')}),
+                # ('Important dates', {'fields': ('last_login', 'date_joined')}),
+            )
+
+    def get_queryset(self, request):
+        """
+        Return the queryset for the admin list view based on user permissions.
+        """
+        qs = super().get_queryset(request)
+
+        # Check if the user is a superuser
+        if not request.user.is_superuser:
+            # Filter the queryset to include only entries related to the logged-in user
+            qs = qs.filter(id=request.user.id)
+
+        return qs
+
 
 admin.site.unregister(Group)
+
+
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     pass
